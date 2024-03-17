b56d7c6c1b81b3486581aba3bbc24bee3137f62f
Yanyutin753 <132346501+Yanyutin753@users.noreply.github.com>
2024-03-18 00:06:05 +0800

    Update style.ts

--- src/components/GalleyGrid/style.ts
+++ src/components/GalleyGrid/style.ts
@@ -2,7 +2,7 @@ import { createStyles } from 'antd-style';
 
 export const MIN_IMAGE_SIZE = 64;
 export const MAX_SIZE_DESKTOP = 320;
-export const MAX_SIZE_MOBILE = 140;
+export const MAX_SIZE_MOBILE = 280;
 export const useStyles = createStyles(
   ({ css }, { col, gap, max, min }: { col: number; gap: number; max: number; min: number }) => ({
     container: css`

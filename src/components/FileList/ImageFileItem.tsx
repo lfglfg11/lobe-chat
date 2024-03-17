ad3fcb7d6a8d71807a6e3dac9ce301df5329312c
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-03-17 15:09:12 +0800

    feat updateFile image

--- src/components/FileList/ImageFileItem.tsx
+++ src/components/FileList/ImageFileItem.tsx
@@ -34,6 +34,16 @@ interface FileItemProps {
   onClick?: () => void;
   style?: CSSProperties;
 }
+const truncateString = (filename: any, maxChars: any) => {
+  if (!filename) return '';
+  if (filename.length > maxChars) {
+    const extension = filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+    // 使用 slice() 函数替代 substring() 函数
+    return filename.slice(0, maxChars) + "." + extension;
+  }
+  return filename;
+}
+
 const ImageFileItem = memo<FileItemProps>(({ editable, id, alwaysShowClose }) => {
   const [useFetchFile, removeFile] = useFileStore((s) => [s.useFetchFile, s.removeFile]);
   const IMAGE_SIZE = editable ? MIN_IMAGE_SIZE : '100%';
@@ -51,7 +61,8 @@ const ImageFileItem = memo<FileItemProps>(({ editable, id, alwaysShowClose }) =>
 
   const imageFileExtensions = new Set([
     '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.ico', '.webp',
-    '.svg', '.eps', '.raw', '.heif', '.heic'
+    '.svg', '.eps', '.raw', '.heif', '.heic', '.xbm', '.tif',
+    '.pjp', '.apng', '.svgz', '.jfif', '.pjpeg', '.avif'
   ]);
 
   const isImage = (filename: string) => {
@@ -64,14 +75,7 @@ const ImageFileItem = memo<FileItemProps>(({ editable, id, alwaysShowClose }) =>
     isShowImage = isImage(data.name);
   }
 
-  const truncateString = (filename: any, maxChars: any) => {
-    if (!filename) return '';
-    if (filename.length > maxChars) {
-      const extension = filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
-      return filename.substring(0, maxChars) + extension;
-    }
-    return filename;
-  }
+  const temFileName = truncateString(data?.name, 4);
 
   if (isShowImage) {
     return (
@@ -99,7 +103,10 @@ const ImageFileItem = memo<FileItemProps>(({ editable, id, alwaysShowClose }) =>
     );
   } else {
     return (
-      <div style={{ display: 'inline-block', position: 'relative' }}>
+      <div style={{
+        display: 'inline-block',
+        fontOpticalSizing: 'auto', fontSize: 'revert-layer', position: 'relative'
+      }}>
         <Image
           actions={
             editable && (
@@ -127,16 +134,15 @@ const ImageFileItem = memo<FileItemProps>(({ editable, id, alwaysShowClose }) =>
           }}
           wrapperClassName={cx(styles.image, editable && styles.editableImage)}
         />
-        <div style={{
-          backgroundColor: 'rgba(255,255,255,0.5)',
-          color: 'black',
-          position: 'absolute',
-          textAlign: 'center',
-          top: '93%',
-          transform: 'translate(0%, -50%)',
-          width: '100%',
-        }}>
-          {truncateString(data?.name || id, 10)}
+        <div style={{ color: '#ffffff', fontWeight: 'bold', left: '50%', position: 'absolute', textAlign: 'center', top: '55%', transform: 'translate(-50%, -50%)', width: '100%' }}>
+          <a
+            href={data?.url}
+            rel="noreferrer noopener"
+            style={{ color: '#ffffff', textDecoration: 'none' }}
+            target="_blank"
+          >
+            {temFileName}
+          </a>
         </div>
       </div>
     );
@@ -144,3 +150,4 @@ const ImageFileItem = memo<FileItemProps>(({ editable, id, alwaysShowClose }) =>
 });
 
 export default ImageFileItem;
+

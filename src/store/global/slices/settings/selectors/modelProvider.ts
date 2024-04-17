ba4f44f9ae23c950518bfb40262c5daef4bfa2d4
Yanyutin753 <132346501+Yanyutin753@users.noreply.github.com>
2024-04-17 21:11:19 +0800

    Revert "如若没找到模型的消耗token,则默认为8192"
    This reverts commit 55b2f90647d931691f523d25d3dbf41c84e3643e.

--- src/store/global/slices/settings/selectors/modelProvider.ts
+++ src/store/global/slices/settings/selectors/modelProvider.ts
@@ -121,8 +121,7 @@ const isModelEnabledUpload = (id: string) => (s: GlobalStore) =>
 const isModelHasMaxToken = (id: string) => (s: GlobalStore) =>
   typeof getModelCardById(id)(s)?.tokens !== 'undefined';
 
-// 如若没找到模型的消耗token,则默认为8192
-const modelMaxToken = (id: string) => (s: GlobalStore) => getModelCardById(id)(s)?.tokens || 8192;
+const modelMaxToken = (id: string) => (s: GlobalStore) => getModelCardById(id)(s)?.tokens || 0;
 
 export const modelProviderSelectors = {
   defaultModelProviderList,

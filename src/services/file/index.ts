f2131f4e598b9990d4deea32a3b82ae1fab9b576
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-06-17 22:38:00 +0800

    Update index.ts

--- src/services/file/index.ts
+++ src/services/file/index.ts
@@ -1,6 +1,9 @@
-import { isServerMode } from '@/const/version';
-
+// import { getClientConfig } from '@/config/client';
 import { ClientService } from './client';
-import { ServerService } from './server';
 
-export const fileService = isServerMode ? new ServerService() : new ClientService();
+// import { ServerService } from './server';
+//
+// const { ENABLED_SERVER_SERVICE } = getClientConfig();
+//
+// export const fileService = ENABLED_SERVER_SERVICE ? new ServerService() : new ClientService();
+export const fileService = new ClientService();

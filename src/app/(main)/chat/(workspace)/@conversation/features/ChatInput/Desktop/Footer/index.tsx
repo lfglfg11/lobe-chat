87d77e67df17498555bd8f109bf78400a7e6b8af
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-07-23 10:54:17 +0800

    Update index.tsx

--- src/app/(main)/chat/(workspace)/@conversation/features/ChatInput/Desktop/Footer/index.tsx
+++ src/app/(main)/chat/(workspace)/@conversation/features/ChatInput/Desktop/Footer/index.tsx
@@ -1,24 +1,21 @@
 import { Icon } from '@lobehub/ui';
-import { Button, Skeleton, Space } from 'antd';
+import { Button, Space } from 'antd';
 import { createStyles } from 'antd-style';
 import { ChevronUp, CornerDownLeft, LucideCommand } from 'lucide-react';
 import { rgba } from 'polished';
-import { memo, useEffect, useState } from 'react';
+import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Center, Flexbox } from 'react-layout-kit';
-
 import StopLoadingIcon from '@/components/StopLoading';
 import SaveTopic from '@/features/ChatInput/Topic';
 import { useSendMessage } from '@/features/ChatInput/useSend';
 import { useAgentStore } from '@/store/agent';
 import { agentSelectors } from '@/store/agent/slices/chat';
 import { useChatStore } from '@/store/chat';
-import { chatSelectors, topicSelectors } from '@/store/chat/selectors';
-import { filesSelectors, useFileStore } from '@/store/file';
+import { chatSelectors } from '@/store/chat/selectors';
 import { useUserStore } from '@/store/user';
 import { modelProviderSelectors, preferenceSelectors } from '@/store/user/selectors';
 import { isMacOS } from '@/utils/platform';
-
 import DragUpload from './DragUpload';
 import { LocalFiles } from './LocalFiles';
 import SendMore from './SendMore';
@@ -40,7 +37,6 @@ const useStyles = createStyles(({ css, prefixCls, token }) => {
         align-items: center;
         justify-content: center;
       }
-
       .${prefixCls}-btn.${prefixCls}-dropdown-trigger {
         &::before {
           background-color: ${rgba(token.colorBgLayout, 0.1)} !important;
@@ -49,6 +45,8 @@ const useStyles = createStyles(({ css, prefixCls, token }) => {
     `,
   };
 });
+
+const isMac = isMacOS();
 
 interface FooterProps {
   setExpand?: (expand: boolean) => void;
@@ -59,21 +57,10 @@ const Footer = memo<FooterProps>(({ setExpand }) => {
 
   const { theme, styles } = useStyles();
 
-  const [
-    isAIGenerating,
-    isHasMessageLoading,
-    isCreatingMessage,
-    isCreatingTopic,
-    stopGenerateMessage,
-  ] = useChatStore((s) => [
+  const [loading, stopGenerateMessage] = useChatStore((s) => [
     chatSelectors.isAIGenerating(s),
-    chatSelectors.isHasMessageLoading(s),
-    chatSelectors.isCreatingMessage(s),
-    topicSelectors.isCreatingTopic(s),
     s.stopGenerateMessage,
   ]);
-
-  const isImageUploading = useFileStore(filesSelectors.isImageUploading);
 
   const model = useAgentStore(agentSelectors.currentAgentModel);
 
@@ -83,37 +70,20 @@ const Footer = memo<FooterProps>(({ setExpand }) => {
   ]);
 
   const sendMessage = useSendMessage();
-
-  const [isMac, setIsMac] = useState<boolean>();
-  useEffect(() => {
-    setIsMac(isMacOS());
-  }, [setIsMac]);
-
   const cmdEnter = (
     <Flexbox gap={2} horizontal>
-      {typeof isMac === 'boolean' ? (
-        <Icon icon={isMac ? LucideCommand : ChevronUp} />
-      ) : (
-        <Skeleton.Node active style={{ height: '100%', width: 12 }}>
-          {' '}
-        </Skeleton.Node>
-      )}
+      <Icon icon={isMac ? LucideCommand : ChevronUp} />
       <Icon icon={CornerDownLeft} />
     </Flexbox>
   );
-
   const enter = (
     <Center>
       <Icon icon={CornerDownLeft} />
     </Center>
   );
-
   const sendShortcut = useCmdEnterToSend ? cmdEnter : enter;
 
   const wrapperShortcut = useCmdEnterToSend ? enter : cmdEnter;
-
-  const buttonDisabled =
-    isImageUploading || isHasMessageLoading || isCreatingTopic || isCreatingMessage;
 
   return (
     <Flexbox
@@ -147,7 +117,7 @@ const Footer = memo<FooterProps>(({ setExpand }) => {
         </Flexbox>
         <SaveTopic />
         <Flexbox style={{ minWidth: 92 }}>
-          {isAIGenerating ? (
+          {loading ? (
             <Button
               className={styles.loadingButton}
               icon={<StopLoadingIcon />}
@@ -158,8 +128,6 @@ const Footer = memo<FooterProps>(({ setExpand }) => {
           ) : (
             <Space.Compact>
               <Button
-                disabled={buttonDisabled}
-                loading={buttonDisabled}
                 onClick={() => {
                   sendMessage();
                   setExpand?.(false);
@@ -168,7 +136,7 @@ const Footer = memo<FooterProps>(({ setExpand }) => {
               >
                 {t('input.send')}
               </Button>
-              <SendMore disabled={buttonDisabled} isMac={isMac} />
+              <SendMore />
             </Space.Compact>
           )}
         </Flexbox>
@@ -176,7 +144,5 @@ const Footer = memo<FooterProps>(({ setExpand }) => {
     </Flexbox>
   );
 });
-
 Footer.displayName = 'Footer';
-
 export default Footer;
